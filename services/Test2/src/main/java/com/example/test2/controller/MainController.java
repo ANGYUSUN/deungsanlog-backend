3cb331bbd470f1c46791e73e06494c3622e64abb
sunny <dksrbtjs9598@naver.com>
2025-05-13 11:15:58 +0900

    Test2 서비스,Scg 서비스추가

--- services/Test2/src/main/java/com/example/test2/controller/MainController.java
+++ services/Test2/src/main/java/com/example/test2/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.test1.controller;
+package com.example.test2.controller;
 
 
 import org.springframework.stereotype.Controller;
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
 
-    @GetMapping("ms1/first")
+    @GetMapping("ms2/second")
     public String mainP(){
        return "main";
     }
