d3d75630bbc4b0c1b4be6e52cbf5b0e10759dadd
sunny <dksrbtjs9598@naver.com>
2025-05-21 16:47:32 +0900

    feature: 백엔드 기초 아키텍처 구축

--- infrastructure/gateway-server/src/main/java/com/example/scg/ScgApplication.java
+++ infrastructure/gateway-server/src/main/java/com/example/scg/ScgApplication.java
@@ -2,8 +2,10 @@ package com.example.scg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ScgApplication {
 
     public static void main(String[] args) {
