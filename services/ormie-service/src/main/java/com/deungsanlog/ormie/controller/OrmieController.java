8ab206d1a8ca84361727ec9c753ce8f1b7c95045
Pushseo <pushseo.kim@gmail.com>
2025-05-28 22:53:05 +0900

    feat: 챗봇 대화 API 추가

--- services/ormie-service/src/main/java/com/deungsanlog/ormie/controller/OrmieController.java
+++ services/ormie-service/src/main/java/com/deungsanlog/ormie/controller/OrmieController.java
@@ -1,17 +1,27 @@
 package com.deungsanlog.ormie.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.deungsanlog.ormie.dto.ChatRequestDto;
+import com.deungsanlog.ormie.dto.ChatResponseDto;
+import com.deungsanlog.ormie.service.OrmieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/ormie")
+@RequiredArgsConstructor
 public class OrmieController {
+
+    private final OrmieService ormieService;
 
     @GetMapping("/status")
     public Map<String, String> getStatus() {
         return Map.of("message", "ormie-service is up!");
+    }
+
+    @PostMapping("/chat")
+    public ChatResponseDto chat(@RequestBody ChatRequestDto requestDto) {
+        return ormieService.ask(requestDto);
     }
 }
