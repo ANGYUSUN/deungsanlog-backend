e202b5ac762ebfe42b6a6424603f23098c7c5b56
Pushseo <pushseo.kim@gmail.com>
2025-05-28 14:15:23 +0900

    refactor: mapping 수정

--- services/meeting-service/src/main/java/com/deungsanlog/meeting/controller/MeetingController.java
+++ services/meeting-service/src/main/java/com/deungsanlog/meeting/controller/MeetingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MeetingController {
 
-    @GetMapping("/meeting")
+    @GetMapping("/meetings")
     public String mainPage() {
         return "main";
     }
