eced3e55032dff08167164d6f3ffc847010c6997
Pushseo <pushseo.kim@gmail.com>
2025-06-26 21:02:57 +0900

    feat(config): 환경(profile)에 따라 ResourceHandler 경로 분기 처리

--- services/community-service/src/main/java/com/deungsanlog/community/config/WebMvcProdConfig.java
+++ services/community-service/src/main/java/com/deungsanlog/community/config/WebMvcProdConfig.java
@@ -1,14 +1,16 @@
 package com.deungsanlog.community.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+@Profile("prod") // 프로필 명시
+public class WebMvcProdConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/api/communities/uploads/**")
-                .addResourceLocations("file:C:/sw-project/deungsanlog-backend/services/community-service/uploads/");
+                .addResourceLocations("file:/home/rocky/backend-deploy/community-service/uploads/");
     }
 }
